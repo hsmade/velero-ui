fa6ca6960da80d589440184932508a6484e6c29e
Wim Fournier <github@fournier.nl>
2023-09-07 22:29:08 +0200

    add error logs

--- api/schedules.go
+++ api/schedules.go
@@ -3,12 +3,14 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/hsmade/velero-ui/internal/schedules"
+	"log/slog"
 	"net/http"
 )
 
 func (a *Api) ListSchedules(c *gin.Context) {
 	result, err := schedules.ListSchedules(c, a.dynamicClient)
 	if err != nil {
+		slog.Error("listing schedules", "err", err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -20,6 +22,7 @@ func (a *Api) GetSchedule(c *gin.Context) {
 	name := c.Param("name")
 	result, err := schedules.GetScheduleDetail(c, a.dynamicClient, name)
 	if err != nil {
+		slog.Error("getting schedule", "err", err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
